0555d62c9f59dc02361b6902e57ed5c4179b649a
ahmed-raed <ahmedraed.dev@gmail.com>
2023-11-27 14:16:57 +0300

    Ready!

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">TaskNest</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList
           todos={todos}
